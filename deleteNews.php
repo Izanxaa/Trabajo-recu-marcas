4f43310a9ad2ef3154181ec73d50fffb2cc5a57c
Izan <ifloressan@educacion.navarra.es>
2025-05-15 19:59:10 +0200

    html y css hecho de borar noticias ademas de su funcionalidad hecha con php

---
+++ deleteNews.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once 'db/dbconnect.php';
+
+    if (isset($_POST['guid'])) {
+        $texto = $_POST['guid'];
+
+        $sqldelete = "delete from entradas_rss where guid like '%$texto%'";
+        $resultado = mysqli_query($connect, $sqldelete);
+
+        if ($resultado) {
+            echo "Han sido eliminadas las noticias que contenian: '$texto'";
+        } else {
+            echo "Error al intentar borrar las noticias";
+        }
+    }
+?>
