d89477564e3c5fbb396a126e4ffcf3eefa342455
Izan <ifloressan@educacion.navarra.es>
2025-05-15 15:44:31 +0200

    conexion a la base de datos hecha

---
+++ db/dbconnect.php
@@ -0,0 +1,19 @@
+<?php
+
+    // Creacion de mis variables
+
+    $host = '127.0.0.1';
+    $port = '3308';
+    $user = 'root';
+    $password = '';
+    $dbname = 'database';
+
+    $connect = mysqli_connect($host, $user, $password, $dbname, $port);
+
+    if (!$connect) {
+        die("Database connection error: "mysqli_connect_error());
+    } else {
+        echo "Successfully connected to de database";
+    }
+
+?>
